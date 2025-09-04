dee09b79fe26fac7831f21e530d526441cf2c03c
Wilfried Klaas <w.klaas@gmx.de>
2025-09-04 14:18:33 +0200

    Refactors geojson export to use go-geom
    Migrates from `paulmach/orb` to `twpayne/go-geom` for geojson export.
    
    This change enhances the geojson export functionality by leveraging the go-geom library. It updates the data structures and methods used for creating geojson features, improving the overall efficiency and maintainability of the geojson exporter.

--- internal/export/geojsonexporter/geojson.go
+++ internal/export/geojsonexporter/geojson.go
@@ -3,8 +3,8 @@ package geojsonexporter
 import (
 	"os"
 
-	"github.com/paulmach/orb"
-	"github.com/paulmach/orb/geojson"
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/encoding/geojson"
 	"github.com/willie68/osmltools/internal/interfaces"
 	"github.com/willie68/osmltools/internal/logging"
 	"github.com/willie68/osmltools/internal/model"
@@ -25,32 +25,50 @@ func New() *GeoJSONExporter {
 func (e *GeoJSONExporter) ExportTrack(track model.Track, outputfile string) error {
 	e.log.Infof("exporting %d loglines to geojson file %s", len(track.LogLines), outputfile)
 
-	coords := make([]orb.Point, 0)
+	coords := make([]geom.Coord, 0)
 	depths := make([]float64, 0)
 	speeds := make([]float64, 0)
 	times := make([]string, 0)
-
 	// collect coordinates, depths, speeds and times
 	for _, wpt := range track.Waypoints {
-		coords = append(coords, orb.Point{wpt.Lon, wpt.Lat})
+		coords = append(coords, geom.Coord{wpt.Lon, wpt.Lat, wpt.Ele})
 		depths = append(depths, wpt.Depth)
 		speeds = append(speeds, wpt.Speed)
 		times = append(times, wpt.Time.Format("2006-01-02T15:04:05Z"))
 	}
 
-	fc := geojson.NewFeatureCollection()
-	f := geojson.NewFeature(orb.LineString(coords))
-	f.Properties["depth"] = depths
-	f.Properties["speed"] = speeds
-	f.Properties["time"] = times
-	f.Properties["name"] = track.Name
-	fc.Append(f)
-	fs := geojson.NewFeature(orb.Point{track.Start.Lon, track.Start.Lat})
-	fs.Properties["name"] = "start"
-	fc.Append(fs)
-	fe := geojson.NewFeature(orb.Point{track.End.Lon, track.End.Lat})
-	fe.Properties["name"] = "end"
-	fc.Append(fe)
+	ls := geom.NewLineString(geom.XYZ)
+	ls.SetCoords(coords)
+	tf := &geojson.Feature{
+		Geometry: ls,
+		Properties: map[string]any{
+			"name":   track.Name,
+			"depths": depths,
+			"speeds": speeds,
+			"times":  times,
+		},
+	}
+	ts := &geojson.Feature{
+		Geometry: geom.NewPointFlat(geom.XY, []float64{track.Start.Lon, track.Start.Lat}),
+		Properties: map[string]any{
+			"name": "start",
+		},
+	}
+	te := &geojson.Feature{
+		Geometry: geom.NewPointFlat(geom.XY, []float64{track.End.Lon, track.End.Lat}),
+		Properties: map[string]any{
+			"name": "end",
+		},
+	}
+
+	fc := geojson.FeatureCollection{
+		Features: []*geojson.Feature{
+			tf,
+			ts,
+			te,
+		},
+	}
+
 	rawJSON, err := fc.MarshalJSON()
 	if err != nil {
 		return err
