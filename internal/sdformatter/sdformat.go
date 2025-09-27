9ca8128bf7d0f3b91891546eacfb860398b0c823
Wilfried Klaas <willie68@users.noreply.github.com>
2025-09-27 12:04:46 +0200

    Add sdformat.go file to the sdformatter directory

---
+++ internal/sdformatter/sdformat.go
@@ -0,0 +1,8 @@
+// This is a sample content for sdformat.go
+
+package sdformatter
+
+// Format function formats the input string.
+func Format(input string) string {
+    return "Formatted: " + input
+}
